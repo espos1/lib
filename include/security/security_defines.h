574bfea02cebdf19d4c5741867281a59257fb40a
fernandathays95-blip <fernandathays95@gmail.com>
2026-06-16 16:51:32 -0300

    Create security_defines.h

---
+++ include/security/security_defines.h
@@ -0,0 +1,13 @@
+/** security_defines.h */
+/** PARTE DE OUTROS */
+#define SECURITY_STATE_LOCKED (0x00001)
+#define __SECURITY__ (0x00002)
+#define STATE_CRITICAL (1 << 0)
+#define STATE_FATAL (1 << 1)
+#define STATE_NORMAL (1 << 2)
+#define STATE_CANCELLD (1 << 2)
+/** API */
+#define API_VERSION 1
+#define API_LEVEL 1
+#define __API__
+#define __API_MAIN__ (__API__)
